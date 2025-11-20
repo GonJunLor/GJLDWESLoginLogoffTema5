b70fbbc592b89369895153b6caec4ee4e8043059
GonJunLor <gonzalo.junlor@educa.jcyl.es>
2025-11-20 10:22:44 +0100

    feat: :sparkles: Inicio

---
+++ indexLoginLogoffTema5.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+    Autor: Gonzalo Junquera Lorenzo
+    Fecha modificación: 20/11/2025
+    Descripción: Indice general del tema 5
+-->
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Indice general de la asignatura">
+    <meta name="author" content="Gonzalo Junquera Lorenzo">
+    <meta name="keywords" content="indice, daw1, edd">
+    <meta name="generator" content="visual studio code">
+    <link rel="icon" type="image/png" href="webroot/media/favicon/favicon-32x32.png">
+    <link rel="stylesheet" href="webroot/css/fonts.css">
+    <link rel="stylesheet" href="webroot/css/estilos.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" type="text/css" rel="stylesheet">
+    <link rel="alternate" href="rss.xml" type="application/rss+xml" title="RSS">
+    <title>Gonzalo Junquera Lorenzo</title>
+</head>
+<body>
+    <div id="aviso">CURSO 2025/2026 -- DAW 2 -- I.E.S. LOS SAUCES</div>
+    <nav>
+        <h2>Proyecto - Login Logoff Tema 5</h2>
+        <h2>Gonzalo Junquera Lorenzo</h2>
+    </nav>
+    <main>
+        
+    </main>
+    <footer id="pie">
+        <div>
+            <a href="https://github.com/GonJunLor/GJLDWESLoginLogoffTema5.git" target="_blank">
+                <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://mogutable.com/" target="_blank"><i class="fa-solid fa-house"></i></a>
+        </div>
+        2024-25 IES LOS SAUCES. &#169;Todos los derechos reservados. 
+        <div>
+            <a href="https://gonzalojunlor.ieslossauces.es/">
+            <address style="display: inline;">Gonzalo Junquera Lorenzo</address>
+            </a>
+            <time datetime="2025-11-20">20-11-2025</time>
+        </div>
+    </footer>
+</body>
+</html>
