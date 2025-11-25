e5bc90959082952dcf6184550d6d3e74f945c701
Gonzalo Junquera Lorenzo <gonzalo.junlor@educa.jcyl.es>
2025-11-25 22:44:36 +0100

    feat: añadir Banderas para elegir idioma

--- indexLoginLogoffTema5.php
+++ indexLoginLogoffTema5.php
@@ -40,10 +40,21 @@
         <img src="webroot/media/images/logo.png" alt="logo">
         <h2>Inicio público</h2>
         <form action="" method="post">
+            <button type="submit" name="idioma" value="ES" id="ES" 
+            style="background-color:<?php echo $_COOKIE["idioma"]=="ES"? '#c2bcb9' : 'white';?>">
+                <img src="webroot/media/images/banderaEs.png" alt="es">
+            </button>
+            <button type="submit" name="idioma" value="EN" id="EN"
+            style="background-color:<?php echo $_COOKIE["idioma"]=="EN"? '#c2bcb9' : 'white';?>">
+                <img src="webroot/media/images/banderaGb.png" alt="en">
+            </button>
+            <button type="submit" name="idioma" value="FR" id="FR"
+            style="background-color:<?php echo $_COOKIE["idioma"]=="FR"? '#c2bcb9' : 'white';?>">
+                <img src="webroot/media/images/banderaFr.png" alt="fr">
+            </button>
+        </form>
+        <form action="" method="post">
             <button name="iniciarSesion" class="boton"><span>Iniciar Sesión</span></button>
-            <input type="submit" value="ES" name="idioma" style="background-color:<?php echo $_COOKIE["idioma"]=="ES"? 'red' : 'lightgray'; ?>">
-            <input type="submit" value="EN" name="idioma" style="background-color:<?php echo $_COOKIE["idioma"]=="EN"? 'red' : 'lightgray'; ?>"> 
-            <input type="submit" value="FR" name="idioma" style="background-color:<?php echo $_COOKIE["idioma"]=="FR"? 'red' : 'lightgray'; ?>">
         </form>
     </nav>
     <main>
